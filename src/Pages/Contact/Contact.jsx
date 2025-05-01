ae42d993696945d66f19e507af47d3f84b36a63f
harshpratik2411 <harshsrivastava2357@gmail.com>
2025-05-01 15:07:15 +0530

    contact banner added

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -5,6 +5,7 @@ import TopNav1 from '../../Components/TopNav1/TopNav1';
 import TopNav2 from '../../Components/TopNav2/TopNav2';
 import Footer from '../../Components/Footer/Footer';
 import location1 from '../../assets/Contact/location.png'
+import banner4 from '../../assets/About/banner4.svg'
 
 const Contact = () => {
   return ( 
@@ -18,20 +19,20 @@ const Contact = () => {
   <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-start">
     {/* Left side: Text content */}
     <div>
-      <p className="text-custom-green text-sm ml-1  font-quicksand font-bold">How can help you ?</p>
-      <h2 className="text-3xl lg:text-4xl font-lato font-bold text-custom-blue mt-2 mb-4">
+      <p  data-aos="fade-up" className="text-custom-green text-sm ml-1  font-quicksand font-bold">How can help you ?</p>
+      <h2 data-aos="fade-up" className="text-3xl lg:text-4xl font-lato font-bold text-custom-blue mt-2 mb-4">
         Let us know how <br /> we can help you
       </h2>
-      <p className="text-custom-text-gray mb-2 max-w-xl">
+      <p data-aos="fade-up" className="text-custom-text-gray mb-2 max-w-xl">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
       </p>
-      <p className="text-custom-text-gray max-w-xl">
+      <p data-aos="fade-up"  className="text-custom-text-gray max-w-xl">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
       </p>
     </div>
 
     {/* Right side: Info grid */}
-    <div className="grid grid-cols-1 mt-16 sm:grid-cols-2 gap-8">
+    <div data-aos="fade-up" className="grid grid-cols-1 mt-16 sm:grid-cols-2 gap-8">
       <div>
         <h4 className="text-custom-blue font-quicksand   font-bold">01. Visit Feedback</h4>
         <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
@@ -53,12 +54,12 @@ const Contact = () => {
 </section>
 
       {/* Map Image */}
-      <div className=" ml-[60px] w-[1350px]">
-        <img src={Banner} alt="Map Banner" className="w-full object-cover" />
+      <div data-aos="fade-up" className=" ml-[60px] w-[1350px]">
+        <img data-aos="fade-up" src={Banner} alt="Map Banner" className="w-full object-cover" />
       </div>
 
       {/* Location Cards */}
-      <section className="grid grid-cols-1 md:grid-cols-3 font-quicksand gap-6 px-6 lg:px-24 py-12 bg-custom-white">
+      <section data-aos="zoomi-in" className="grid grid-cols-1 md:grid-cols-3 font-quicksand gap-6 px-6 lg:px-24 py-12 bg-custom-white">
         {['Office', 'Studio', 'Shop'].map((location, index) => (
           <div key={index}>
             <h4 className="text-custom-green font-bold text-lg mb-2">{location}</h4>
@@ -76,11 +77,11 @@ const Contact = () => {
       </section>
 
       {/* Contact Form */}
-      <section className="px-6 lg:px-24 py-12 grid grid-cols-1 lg:grid-cols-3 gap-8">
+      <section  className="px-6 lg:px-24 py-12 grid grid-cols-1 lg:grid-cols-3 gap-8">
         <div className="lg:col-span-2">
-          <p className="text-custom-green font-quicksand ml-1 text-sm font-bold">Contact form</p>
-          <h2 className="text-3xl mt-2 font-quicksand  font-bold text-custom-blue mb-4">Drop Us a Line</h2>
-          <p className="text-sm text-custom-text-lightgray mb-6">Your email address will not be published. Required fields are marked *</p>
+          <p  data-aos="fade-up" className="text-custom-green font-quicksand ml-1 text-sm font-bold">Contact form</p>
+          <h2 data-aos="fade-up"  className="text-3xl mt-2 font-quicksand  font-bold text-custom-blue mb-4">Drop Us a Line</h2>
+          <p data-aos="fade-up" className="text-sm text-custom-text-lightgray mb-6">Your email address will not be published. Required fields are marked *</p>
 
           <form className="space-y-4">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -97,8 +98,27 @@ const Contact = () => {
         <div className="hidden lg:block">
           <img src={Img} alt="Woman on call" className="w-full h-auto rounded-lg object-cover" />
         </div>
-      </section>
-    </div>  
+      </section> 
+    </div>   
+   <section className="relative mb-16  lg:w-[1490px] lg:h-[460px] sm:w-[790px] sm:h-[450px]  mt-4 rounded-lg py-16 px-8 flex flex-wrap justify-center items-center">
+             <img src={banner4} alt="" className="absolute  rounded-lg inset-0 w-full  h-full sm:object-cover lg:object-cover" />
+             <div className="relative max-w-4xl text-center  text-white">
+               <h1 className="lg:text-5xl sm:text-2xl font-quicksand lg:-ml-[380px] ml-[10px] font-bold lg:w-[650px] lg:h-[130px] sm:w-[380px] sm:h-[80px] text-custom-blue py-4 mx-auto text-left">
+                Stay home & Get your daily <br /> needs from our  shop
+               </h1>
+               <p className="lg:text-xl sm:text-base font-lato text-custom-new-gray lg:-ml-[760px] sm:-ml-[300px] lg:mr-0  mr-[112px] cursor-pointer  mb-6">Sign up for the daily newsletter</p>
+               <div className="flex justify-center items-center lg:-ml-[390px] mr-3  mt-2 bg-custom-white rounded-full shadow-md lg:max-w-md sm:max-w-sm mx-auto overflow-hidden">
+                 <input
+                   type="email"
+                   placeholder="Your email address"
+                   className="flex-1 px-4  py-3 outline-none  text-custom-blue"
+                   /> 
+                   <button className="bg-custom-green   lg:w-auto lg:ml-2  cursor-pointer text-custom-white px-6 py-3 rounded-full font-medium">
+                   Subscribe
+                 </button> 
+                </div>
+             </div>
+           </section> 
     <Footer/>
     </>
   );
